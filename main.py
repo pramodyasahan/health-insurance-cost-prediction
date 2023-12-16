efb44a2b1ad60cd27007a300840430510b9031ff
pramodyasahan <sahandil1013@gmail.com>
2023-12-16 17:03:02 +0530

    feat: print prediction and test result

--- main.py
+++ main.py
@@ -32,4 +32,7 @@ lin_reg.fit(X_poly_train, y_train)
 
 y_pred = lin_reg.predict(X_poly_test)
 
+np.set_printoptions(precision=2)
+print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)), 1))
+
 
