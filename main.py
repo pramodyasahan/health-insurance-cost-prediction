da055e87ef7b94ce860ce3a086321bf93b873aad
pramodyasahan <sahandil1013@gmail.com>
2023-12-16 16:33:27 +0530

    feat: import necessary libraries

---
+++ main.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVR
+
+
+
