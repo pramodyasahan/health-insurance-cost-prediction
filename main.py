18609aa0f2fb7304f20d956ea532db1c9354b68b
pramodyasahan <sahandil1013@gmail.com>
2023-12-16 16:38:54 +0530

    feat: split the dataset in to train and test

--- main.py
+++ main.py
@@ -8,3 +8,4 @@ dataset = pd.read_csv('insurance.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
