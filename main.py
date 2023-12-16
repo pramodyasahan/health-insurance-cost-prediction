eecc147c2d90593b9e773e0c9f885f3f9361b072
pramodyasahan <sahandil1013@gmail.com>
2023-12-16 16:33:42 +0530

    feat: import the data-set

--- main.py
+++ main.py
@@ -4,5 +4,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVR
 
-
+dataset = pd.read_csv('insurance.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, -1].values
 
