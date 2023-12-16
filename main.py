acb7ffcb8e375dab3e95c925236707d24bfe9884
pramodyasahan <sahandil1013@gmail.com>
2023-12-16 16:43:31 +0530

    feat: use column transfer for nominal data

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.svm import SVR
 
 dataset = pd.read_csv('insurance.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
+
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [1, 4, 5])], remainder='passthrough')
+X = np.array(ct.fit_transform(X))
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
